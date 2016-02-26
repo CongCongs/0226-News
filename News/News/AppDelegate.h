fea1dbd76dd6e60b67e3cef3531f031d6629bfa7
congcong <690994470@qq.com>
2016-02-26 18:46:47 +0800

    新建项目

---
+++ News/News/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  News
+//
+//  Created by 陈锦聪 on 16/2/26.
+//  Copyright © 2016年 Cong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ News/News/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  News
+//
+//  Created by 陈锦聪 on 16/2/26.
+//  Copyright © 2016年 Cong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
